b0dc7796727a43d1101370df979bdd0221e156aa
Simon <simon.perols@oweb.pm>
2020-03-23 18:20:09 +0100

    feat: add sonarqube auto scan by gulp task

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sonarqubeScanner = require('sonarqube-scanner');
+
+gulp.task('sonar', function(callback) {
+    sonarqubeScanner({
+        serverUrl : process.env.SONARQUBE_URL,
+        token: process.env.SONARQUBE_TOKEN,
+        options : {
+            "sonar.organization": process.env.SONARQUBE_ORGANIZATION,
+            "sonar.projectKey": process.env.SONARQUBE_PROJECTKEY,
+            "sonar.javascript.lcov.reportPaths": "build/test-results/lcov.info"
+        }
+    }, callback);
+});
